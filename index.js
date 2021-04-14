0b17d333b534cb955d4708e254c0bb22215a1131
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2021-04-13 22:23:25 -0700

    Added 2 GET and 1 post endpoints

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// require your server and launch it here
+const express = require('express');
+const postsRouter = require("./api/posts/posts-router");
+
+const server = express();
+
+
+server.use(express.json())
+server.use(postsRouter)
+
+
+server.get('/', (req, res) => {
+
+    res.send('Hello World');
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
